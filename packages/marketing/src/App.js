bb66375ed60343589b91229787c275cd0ea9bc1f
Trần Mạnh Hưng <tranhung26122612@gmail.com>
2025-01-14 11:49:22 +0700

    marketing micro FE

---
+++ packages/marketing/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
+import Pricing from './components/Pricing';
+import Landing from './components/Landing';
+
+const App = () => {
+  const generateClassNames = createGenerateClassName({
+    productionPrefix: 'ma',
+  });
+
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassNames}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
+  );
+};
+
+export default App;
